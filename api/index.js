3b68144118988eedc7ffcc7660d8db0053106769
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-09-27 16:58:17 +0500

    Implement mock server

--- api/index.js
+++ api/index.js
@@ -1,1 +1,30 @@
-console.log( "E-commerce" )
+import express from "express"
+import cors from "cors"
+
+const server = express()
+server.use( cors() )
+server.use( express.json() )
+
+server.get( "/categories", ( _, res ) => {
+
+	res.send( [
+		{
+			id: 1,
+			name: "Electronics"
+		}
+	] )
+} )
+
+server.get( "/products/:cid", ( _, res ) => {
+
+	res.send( [
+		{
+			id: 1,
+			name: "iPhone 15 Pro",
+			price: 1299,
+			img_url: "/image/no_image.png"
+		}
+	] )
+} )
+
+server.listen( 4_000, () => console.info( ":4000" ) )

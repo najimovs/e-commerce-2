27e9e4147dd62fe5db6df3079fd34b11a90d5211
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-09-29 15:47:45 +0500

    Fix package.json

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { executeQuery } from "./db.js"
+import { executeQuery } from "./utils/db.js"
 
 const server = express()
 server.use( cors() )
